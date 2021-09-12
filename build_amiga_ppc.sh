46fac9dfc1c117dc1e7c4138ec487bd0d5b25e0a
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-09-12 19:07:47 +0200

    Work on the build system

--- build_amiga_ppc.sh
+++ build_amiga_ppc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make CC="gcc -fno-peephole2" CXX="g++" AS=as AR=ar RANLIB=ranlib PLATFORM=AMIGA AMISUFFIX=".ppc"  $*
+make CC="gcc -fno-peephole2" CXX="g++" AS=as AR=ar RANLIB=ranlib PLATFORM=AMIGAPPC AMISUFFIX=".ppc"  $*
